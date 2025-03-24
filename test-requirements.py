fd5d522dfd7477a509f4915448b3f8415c0e6510
naveenkumarbajantri <107464456+naveenkumarbajantri@users.noreply.github.com>
2025-03-24 11:29:52 +0530

    Update test-requirements.py

--- test-requirements.py
+++ test-requirements.py
@@ -2,7 +2,7 @@ import mlflow.pyfunc
 import os
 
 # Point directly to the saved model location (relative to Jenkins workspace)
-MODEL_PATH = "train/mlruns/6021/a3b1ea226ea646aeb0fa88ca97980086/artifacts/model"
+MODEL_PATH = "train/mlruns/602170594853194749/a3b1ea226ea646aeb0fa88ca97980086/artifacts/model"
 
 # Load model
 model = mlflow.pyfunc.load_model(MODEL_PATH)
